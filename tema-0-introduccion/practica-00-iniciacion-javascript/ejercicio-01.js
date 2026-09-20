cf3bb0119db1263752c5d90bcfb610b227a22136
diegoalegil <diegogildam@gmail.com>
2026-09-20 22:44:23 +0100

    Completa el ejercicio 1 de la práctica 00

---
+++ tema-0-introduccion/practica-00-iniciacion-javascript/ejercicio-01.js
@@ -0,0 +1,13 @@
+console.log("Hola JavaScript");
+
+console.log("Estoy preparando mi primera pokedex :D");
+console.log("Durante el curso usaremos JavaScript y TypeScript");
+
+const nombre = "Celebi";
+const tipo = "Planta";
+const nivel = 50;
+
+console.log(nombre);
+console.log(tipo);
+console.log(nivel);
+console.log(`${nombre} es de tipo ${tipo} y tiene el nivel ${nivel}`);
